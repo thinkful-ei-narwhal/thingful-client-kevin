6c54ebc1d408e5ca113b9c31863e88e42c0cfb0b
Kevin Jiang <kjiang95@gmail.com>
2020-05-15 12:47:05 -0500

    first commit

---
+++ src/services/thing-api-service.js
@@ -0,0 +1,63 @@
+import config from '../config'
+import TokenService from './token-service'
+
+
+const ThingApiService = {
+  getThings() {
+    return fetch(`${config.API_ENDPOINT}/things`, {
+      headers: {
+        'Authorization': `basic ${TokenService.getAuthToken()}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getThing(thingId) {
+    return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
+      headers: {
+        'Authorization': `basic ${TokenService.getAuthToken()}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getThingReviews(thingId) {
+    return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
+      headers: {
+        'Authorization': `basic ${TokenService.getAuthToken()}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postReview(thingId, text, rating) {
+    return fetch(`${config.API_ENDPOINT}/reviews`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `basic ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify({
+        thing_id: thingId,
+        rating,
+        text,
+      }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+}
+
+export default ThingApiService
